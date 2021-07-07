7ec1ef652787f023bf16b8f78ed7a5def738f437
deds129 <nik.chudinov129@mail.ru>
2021-07-08 00:22:01 +0300

    Start Security part

--- src/main/java/com/tupolev/application/WebAppController.java
+++ src/main/java/com/tupolev/application/WebAppController.java
@@ -8,32 +8,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 import java.util.Map;
 
 
 
 @Controller
-public class GreetingController {
+public class WebAppController {
     @Autowired
     private WorkerRepo workerRepo;
 
-    @GetMapping("/greeting")
-    public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World") String name,
-            Map<String, Object> model) {
-        model.put("name", name);
+    //main page
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
+
         return "greeting";
     }
 
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map<String, Object> model) {
         Iterable<Worker> workers = workerRepo.findAll();
         model.put("workers",workers);
         return "main";
     }
 
-    @PostMapping
+    @PostMapping("/main")
     public String add(@RequestParam String workerName, Map<String, Object> model){
         //save new worker
         Worker worker = new Worker(workerName);
@@ -45,7 +43,7 @@ public class GreetingController {
         return "main";
     }
 
-    @PostMapping("filter")
+    @PostMapping("/filter")
     public String filter(@RequestParam String filter, Map<String, Object> model){
         Iterable<Worker> workers;
         if (filter != null && !filter.isEmpty())
